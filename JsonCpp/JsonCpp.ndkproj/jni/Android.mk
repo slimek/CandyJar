590079727ed938b6b0656d79311d929d09386880
slimek <slime573@gmail.com>
2013-11-04 11:03:26 +0800

    Add JsonCpp NDK project.

---
+++ JsonCpp/JsonCpp.ndkproj/jni/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := JsonCpp
+
+MY_SRC_DIR := ../..
+
+LOCAL_SRC_FILES := \
+	$(MY_SRC_DIR)/json_reader.cpp \
+	$(MY_SRC_DIR)/json_value.cpp \
+	$(MY_SRC_DIR)/json_writer.cpp
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/../.. \
+	$(LOCAL_PATH)/../../../include
+
+include $(BUILD_STATIC_LIBRARY)
