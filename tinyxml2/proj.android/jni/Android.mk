eaaf105ba5a589b75f45df4be09797a60f78d992
slimek <slime573@gmail.com>
2013-11-11 09:21:13 +0800

    Rearrange Tinyxml2 folder structure.

--- tinyxml2/proj.android/jni/Android.mk
+++ tinyxml2/proj.android/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := tinyxml2-static
 LOCAL_MODULE_FILENAME := tinyxml2
 
-LOCAL_SRC_FILES := ../../tinyxml2.cpp
+LOCAL_SRC_FILES := ../../src/tinyxml2.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include/tinyxml2
 
 include $(BUILD_STATIC_LIBRARY)
