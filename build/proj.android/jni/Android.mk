93981d844f15570d809caa7e821a5e29d03e85bf
slimek <slime573@gmail.com>
2013-11-13 01:11:21 +0800

    [build] Update NDK make file.

--- build/proj.android/jni/Android.mk
+++ build/proj.android/jni/Android.mk
@@ -1,4 +1,10 @@
-LOCAL_PATH := $(call my-dir)
+MY_LOCAL_PATH := $(call my-dir)
+
+include ../../Boost.Chrono/proj.android/jni/Android.mk
+
+include ../../Boost.System/proj.android/jni/Android.mk
+
+LOCAL_PATH := MY_LOCAL_PATH
 
 include $(CLEAR_VARS)
 
@@ -6,39 +12,15 @@ LOCAL_MODULE := CandyJar
 
 LOCAL_WHOLE_STATIC_LIBRARIES := \
 	boost-chrono \
-	boost-date_time \
-	boost-filesystem \
-	boost-program_options \
-	boost-regex \
-	boost-system \
-	JsonCpp \
-	tinyxml2
+	boost-system
+
+CANDY_JAR_LIB := ../../lib
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-add-path,$(GIT_ROOT)/CandyJar)
-$(call import-module,Boost.Chrono/proj.android/jni)
-$(call import-module,Boost.DateTime/proj.android/jni)
-$(call import-module,Boost.Filesystem/proj.android/jni)
-$(call import-module,Boost.ProgramOptions/proj.android/jni)
-$(call import-module,Boost.Regex/proj.android/jni)
-$(call import-module,Boost.System/proj.android/jni)
-$(call import-module,JsonCpp/proj.android/jni)
-$(call import-module,tinyxml2/proj.android/jni)
+$(LOCAL_BUILT_MODULE): copy-work-$(TARGET_ARCH_ABI)
 
+copy-work-$(TARGET_ARCH_ABI):
+	$(call host-mkdir,$(CANDY_JAR_LIB)/Android.Ndk)
+	$(call host-mkdir,$(CANDY_JAR_LIB)/Android.Ndk/$(TARGET_ARCH_ABI))
 
-
-include $(CLEAR_VARS)
-
-#
-# Copy the built result to lib directories
-#
-
-
-MY_LIB_NDK := "$(GIT_ROOT)\CandyJar\lib\Android.Ndk"
-MY_LIB_ABI := "$(MY_LIB_NDK)\$(TARGET_ARCH_ABI)"
-
-$(shell if not exist $(MY_LIB_NDK) (mkdir $(MY_LIB_NDK)))
-$(shell if not exist $(MY_LIB_ABI) (mkdir $(MY_LIB_ABI)))
-
-NOISE := $(shell copy obj\local\$(TARGET_ARCH_ABI)\*.a $(MY_LIB_ABI))
