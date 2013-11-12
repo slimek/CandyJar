8b9868a258e930437eb45d00b3c06e263b594aad
jeffyin <yin75620@hotmail.com>
2013-11-12 16:19:23 +0800

    讓編譯通過

---
+++ Boost.DateTime/proj.android/jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := boost-datetime
+
+MY_SRC_DIR := ../../src
+
+LOCAL_SRC_FILES := \
+	$(MY_SRC_DIR)/posix_time/posix_time_types.cpp \
+	$(MY_SRC_DIR)/gregorian/date_generators.cpp \
+	$(MY_SRC_DIR)/gregorian/greg_month.cpp \
+	$(MY_SRC_DIR)/gregorian/greg_weekday.cpp \
+	$(MY_SRC_DIR)/gregorian/gregorian_types.cpp 
+	
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/../../../include
+	
+include $(BUILD_STATIC_LIBRARY)
