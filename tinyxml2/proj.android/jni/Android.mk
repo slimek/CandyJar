d48f4cf62b9c21662f2df2e5dc3f40e38775a171
slimek <slime573@gmail.com>
2013-11-12 12:35:09 +0800

    [tinyxml2] Remove Application.mk and change module name to tinyxml2.

--- tinyxml2/proj.android/jni/Android.mk
+++ tinyxml2/proj.android/jni/Android.mk
@@ -2,8 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := tinyxml2-static
-LOCAL_MODULE_FILENAME := tinyxml2
+LOCAL_MODULE := tinyxml2
 
 LOCAL_SRC_FILES := ../../src/tinyxml2.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include/tinyxml2
