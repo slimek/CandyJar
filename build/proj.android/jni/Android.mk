db154480282a31aae36d89c6b04463a0303f7e23
slimek <slime573@gmail.com>
2013-11-13 10:42:03 +0800

    [build/NDK] Can clean output files and validate some NDK macros. - Remove redundant CandyJar.cpp

--- build/proj.android/jni/Android.mk
+++ build/proj.android/jni/Android.mk
@@ -10,8 +10,13 @@ MY_LOCAL_PATH := $(call my-dir)
 # Build each libraries
 #
 
-include ../../Boost.Chrono/proj.android/jni/Android.mk
-include ../../Boost.System/proj.android/jni/Android.mk
+# $(call candy-make,lib-dir)
+define candy-make
+../../$1/proj.android/jni/Android.mk
+endef
+
+include $(call candy-make,Boost.Chrono)
+include $(call candy-make,Boost.System)
 
 
 #
@@ -31,12 +36,12 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
 
 #
 # Make output directories, then copy all built libraries
 #
 
+# My variables
 CANDY_LIB := ../../lib
 MY_ARCH_ABI := $(TARGET_ARCH_ABI)
 
@@ -45,13 +50,48 @@ define candy-copy
   $(call host-cp,obj/local/$@/$1,$(CANDY_LIB)/Android.Ndk/$@/$1)
 endef
 
+all: $(MY_ARCH_ABI)
 
-$(LOCAL_BUILT_MODULE): $(MY_ARCH_ABI)
-
-$(MY_ARCH_ABI):
+$(MY_ARCH_ABI): $(LOCAL_BUILT_MODULE)
 	$(call host-mkdir,$(CANDY_LIB)/Android.Ndk)
 	$(call host-mkdir,$(CANDY_LIB)/Android.Ndk/$@)
-	$(call candy-copy,libboost-chrono.a)	
+	$(call candy-copy,libboost-chrono.a)
+	$(call candy-copy,libboost-system.a)
 
 
+#
+# Clean the output directories
+#
 
+# $(call candy-clean,lib-name)
+define candy-clean
+  $(call host-rm,$(CANDY_LIB)/Android.Ndk/$(subst -clean,,$@)/$1.a)
+endef
+
+clean: $(MY_ARCH_ABI)-clean
+
+$(MY_ARCH_ABI)-clean:
+	$(call candy-clean,libboost-chrono)
+	$(call candy-clean,libboost-system)
+
+
+#
+# Appendix
+# : Validate all necessary NDK undocumented macros are defined 
+#
+
+ifndef LOCAL_BUILT_MODULE
+$(error LOCAL_BUILT_MODULE not defined)
+endif
+
+ifndef host-mkdir
+$(error host-mkdir not defined)
+endif
+
+ifndef host-cp
+$(error host-cp not defined)
+endif
+
+ifndef host-rm
+$(error host-rm not defined)
+endif
