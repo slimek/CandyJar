d38ed15b166a494bdb673248b8d1ea6ed425e0b4
slimek <slime573@gmail.com>
2013-11-15 18:01:26 +0800

    [NDK] (minor) Doesn't need to specify Cocos2d-x debug flag.

--- build/proj.android/jni/Application.mk
+++ build/proj.android/jni/Application.mk
@@ -3,7 +3,7 @@ APP_ABI := x86 armeabi armeabi-v7a
 APP_STL := gnustl_static
 APP_OPTIM := debug
  
-APP_CFLAGS += -fexceptions -DCOCOS2D_DEBUG=2
-APP_CPPFLAGS += -std=c++11 -frtti -fexceptions -DCOCOS2D_DEBUG=2
+APP_CFLAGS += -fexceptions
+APP_CPPFLAGS += -std=c++11 -frtti -fexceptions
  
 NDK_TOOLCHAIN_VERSION := 4.8
