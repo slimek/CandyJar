fe0dd80e00859c6077df253868c81f2968279c8e
slimek <slime573@gmail.com>
2013-12-16 17:06:51 +0800

    [NDK] Split libraries to Debug/Release. - Add -j 4 make option.

--- build/proj.android/jni/Application.mk
+++ build/proj.android/jni/Application.mk
@@ -1,9 +1,25 @@
 APP_PLATFORM := android-10
 APP_ABI := x86 armeabi armeabi-v7a
 APP_STL := gnustl_static
+
+APP_CFLAGS := -fexceptions
+APP_CPPFLAGS := -std=c++11 -frtti
+
+ifeq ($(NDEBUG),1)
+
+# Release
+APP_OPTIM := release
+CANDY_CONFIG := Release
+
+else
+
+# Debug
 APP_OPTIM := debug
- 
-APP_CFLAGS += -fexceptions
-APP_CPPFLAGS += -std=c++11 -frtti -fexceptions
- 
+CANDY_CONFIG := Debug
+
+APP_CFLAGS += -g -DCOCOS2D_DEBUG=1
+
+endif
+
+NDK_APP_OUT := obj/$(CANDY_CONFIG)
 NDK_TOOLCHAIN_VERSION := 4.8
